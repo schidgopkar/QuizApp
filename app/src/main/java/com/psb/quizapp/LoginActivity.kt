0535ba5d42b22caddcaa69388b01bad2e84ea49d
shrikantchidgopkar <shri.c@hotmail.com>
2020-01-22 15:15:19 +0800

    Firebase Login and Sign up UI completed. Changed app theme colours to match PSB colours

--- app/src/main/java/com/psb/quizapp/LoginActivity.kt
+++ app/src/main/java/com/psb/quizapp/LoginActivity.kt
@@ -1,10 +1,81 @@
 package com.psb.quizapp
 
-class LoginActivity {
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
 
 
 
-    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        signOut()
+        displaySignInOptions()
+    }
 
 
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+
+                startActivity(Intent(this, MainActivity::class.java))
+
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    private fun displaySignInOptions() {
+
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // [START auth_fui_theme_logo]
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.psb_logo) // Set logo drawable
+                .setTheme(R.style.AppTheme) // Set theme
+                .build(),
+            RC_SIGN_IN)
+        // [END auth_fui_theme_logo]
+    }
+
+
+    private fun signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                // ...
+            }
+        // [END auth_fui_signout]
+    }
+
+    companion object {
+
+        private const val RC_SIGN_IN = 123
+    }
 }
