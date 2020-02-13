5468e6d9d56f934b123d723581c3e979b757c15e
shrikantchidgopkar <shri.c@hotmail.com>
2020-02-13 12:32:03 +0800

    Expresso testing

--- app/src/androidTest/java/com/psb/quizapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/psb/quizapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,31 @@
 package com.psb.quizapp
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+
+
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import org.hamcrest.Matchers.not
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+
+import org.hamcrest.CoreMatchers.not
+
+
+
+
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,6 +33,7 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
@@ -21,4 +41,76 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.psb.quizapp", appContext.packageName)
     }
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun testCorrectQuesTextIsBeingDisplayed(){
+
+        val questionModel = QuestionListViewModel()
+
+        questionModel.getQuestionsData {
+
+        }
+
+        Thread.sleep(5000)
+
+        onView(withId(R.id.question_text_view))
+            .check(matches(withText(questionModel.questions.first().text)))
+
+
+    }
+
+    @Test
+    fun testFirstQuestionCorrectOptionTextBeingDisplayedForRadioButtonOptionA(){
+
+        val questionModel = QuestionListViewModel()
+
+        questionModel.getQuestionsData {
+
+        }
+
+        Thread.sleep(5000)
+
+        onView(withId(R.id.radio_1))
+            .check(matches(withText(questionModel.questions.first().optionA)))
+
+    }
+
+
+
+    @Test
+    fun testFirstQuestion_CorrectAnswer_DisplaysCorrectAnswerNotification(){
+
+        val questionModel = QuestionListViewModel()
+
+        questionModel.getQuestionsData {
+
+        }
+
+        Thread.sleep(5000)
+
+        var clickID = R.id.radio_1
+
+        if (questionModel.questions.first().answer == "b"){
+            clickID = R.id.radio_2
+        }
+        if (questionModel.questions.first().answer == "c"){
+            clickID = R.id.radio_3
+        }
+        if (questionModel.questions.first().answer == "d"){
+            clickID = R.id.radio_4
+        }
+
+        onView(withId(clickID)).perform(click())
+
+        onView(withText("Great. It is the correct answer. Current score is 1")).inRoot(withDecorView(org.hamcrest.Matchers.not(org.hamcrest.Matchers.`is`(activityRule.activity.window.decorView))))
+            .check(matches(isDisplayed()))
+        
+    }
+
+
+
+
 }
--- app/src/test/java/com/psb/quizapp/ExampleUnitTest.kt
+++ app/src/test/java/com/psb/quizapp/ExampleUnitTest.kt
@@ -3,22 +3,24 @@ package com.psb.quizapp
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+
 class ExampleUnitTest {
 
-    private lateinit var questionListViewModel: QuestionListViewModel
 
 
 
     @Test
     fun addition_isCorrect() {
 
-        questionListViewModel = QuestionListViewModel()
+
 
 
     }
